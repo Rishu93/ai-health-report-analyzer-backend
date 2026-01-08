22781b9df5ab8425e30f23b61e5363b0a94d0ff7
riteshsingh03-dot <iamriteshsingh03@gmail.com>
2026-01-08 16:35:09 +0530

    Added a intro page.

--- script.js
+++ script.js
@@ -22,3 +22,27 @@ function analyseData() {
     resultBox.innerHTML = "Backend not connected (demo mode)";
   });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const introPage = document.getElementById("introPage");
+  const appPage = document.getElementById("appPage");
+  const startBtn = document.getElementById("startBtn");
+
+  startBtn.addEventListener("click", function () {
+
+    // Fade out intro
+    introPage.classList.add("fade-out");
+
+    setTimeout(function () {
+      introPage.style.display = "none";
+
+      // Show app + fade in
+      appPage.style.display = "flex";
+      appPage.classList.add("fade-in");
+
+    }, 400); // must match fadeOut duration
+  });
+
+});
+
